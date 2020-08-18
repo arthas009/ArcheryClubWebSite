11f31d5204e7e164a9fe6f1dcfec424a30195e3e
Yusuf <yusufalti1997@gmail.com>
2020-08-18 19:59:03 +0300

    Added features
    Added left-side menu.
    Added log-in button.
    Added some other attributes for old elements.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import Blog from "./material-ui/Blog"
 import * as serviceWorker from './serviceWorker';
 
